41e2003d20f476753270e5c9437f632acda881f8
vidankov <slavavinsent@gmail.com>
2025-03-07 10:59:14 +0300

    в проект добавлен файл с изображением заглушкой, пока грузятся данные картинки

--- components/HomePage/components/CarouselItemImage.jsx
+++ components/HomePage/components/CarouselItemImage.jsx
@@ -1,4 +1,4 @@
-function CarouselItemImage() {
+function CarouselItemImage({ title }) {
     /*
         Для генерации фонов:
         https://dummyjson.com/image/400x200/008080/{код цвета}?text={текст}+{текст},
@@ -6,11 +6,11 @@ function CarouselItemImage() {
         https://dummyjson.com/image/400x200/008080/ffffff?text=Hello+Peter
     */
 
-    //const [imgSource, setImgSource] = useState(null);
+    const [imgSource, setImgSource] = useState(null);
 
-    //if (!imgSource) {
-    //    return <div>Изображение загружается...</div>
-    //}
+    if (!imgSource) {
+        return <div>Изображение загружается...</div>
+    }
 
     return (
         <img src="https://loremflickr.com/1280/720" className="d-block w-100" alt="..." />
