ed8e156cfca4dff4da43cbb8ae5d97513037fd7a
vidankov <slavavinsent@gmail.com>
2025-03-07 13:51:41 +0300

    Доработано наполнение CarouselItem: стилизован блок информации о блюде, добавлен список ингридиентов и способ приготовления

--- components/HomePage/components/CarouselItem.jsx
+++ components/HomePage/components/CarouselItem.jsx
@@ -1,14 +1,32 @@
-import CarouselItemImage from "./CarouselItemImage";
-
 function CarouselItem(props) {
     // console.log(`"id элемента ${props.item.id}, isActive: ${props.isActive}"`);
+    const textShadowStyle = { textShadow: "2px 2px 4px rgba(0, 0, 0, 0.5)" };
     return (
         <div className={`carousel-item${props.isActive ? ' active' : ''}`}>
             {/*<CarouselItemImage title={props.item ? props.item.name : "Изображение загружается..."} />*/}
-            <img src={props.item.image} className="d-block w-100" alt="..." />
-            <div className="carousel-caption d-none d-md-block">
-                <h5>Метка первого слайда</h5>
-                <p>Некоторый репрезентативный заполнитель для первого слайда.</p>
+            <img src={props.item.image} className="d-block w-100" alt={props.item.name} />
+            <div className=" bg-dark bg-opacity-50 rounded carousel-caption d-none d-md-block">
+                <h2 style={textShadowStyle}>{props.item.name}</h2>
+                <div className="fs-4 container" style={textShadowStyle}>
+                    <div className="row align-items-start">
+                        <ul className="col">{
+                            props.item.ingredients.map((ingredient, index) =>
+                                <li className="list-group-item text-start" key={`${props.item.name}-${ingredient}`}>
+                                    {index + 1}) {ingredient}
+                                </li>
+                            )
+                        }
+                        </ul>
+                        <ul className="col">{
+                            props.item.instructions.map(instruction =>
+                                <li className="list-group-item" key={`${props.item.name}-${instruction}`}>
+                                    {instruction}
+                                </li>
+                            )
+                        }
+                        </ul>
+                    </div>
+                </div>
             </div>
         </div>
     );
