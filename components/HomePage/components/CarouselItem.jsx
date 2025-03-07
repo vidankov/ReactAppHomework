a439d4598319fac53c7811ca3a8332bdada3364a
vidankov <slavavinsent@gmail.com>
2025-03-07 12:54:04 +0300

    В качестве картинок для объектов карусели использованы реальные картинки из входящих данных вместо функционала со случайной генерацией

--- components/HomePage/components/CarouselItem.jsx
+++ components/HomePage/components/CarouselItem.jsx
@@ -4,7 +4,8 @@ function CarouselItem(props) {
     // console.log(`"id элемента ${props.item.id}, isActive: ${props.isActive}"`);
     return (
         <div className={`carousel-item${props.isActive ? ' active' : ''}`}>
-            <CarouselItemImage title={props.item ? props.item.name : "Изображение загружается..."} />
+            {/*<CarouselItemImage title={props.item ? props.item.name : "Изображение загружается..."} />*/}
+            <img src={props.item.image} className="d-block w-100" alt="..." />
             <div className="carousel-caption d-none d-md-block">
                 <h5>Метка первого слайда</h5>
                 <p>Некоторый репрезентативный заполнитель для первого слайда.</p>
